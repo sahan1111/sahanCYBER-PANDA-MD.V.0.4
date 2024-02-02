11714fc07bdf0905deb1235dd936daeb0a1cfaf9
Dark_Sahan-x_team <120746548+sahan1111@users.noreply.github.com>
2024-02-02 13:25:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,15 +21,15 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94772108460", "+94772108460"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94772108460", "+94772108460"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94772108460"];
-global.OwnerName = "KUSHAN";
-global.BotName = "DARK SHAM";
-global.packname = "🦋 DARK SHAN 🦋";                             //Do not change.
-global.author = "KUSHAN";                               //Do not change.
+global.Owner = ["+94789281290", "+94756294146"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+94756294146", "+94789281290"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+94789281290"];
+global.OwnerName = "Sahan-x";
+global.BotName = "DARK_Alvin";
+global.packname = "🦋 DARK_Alvin 🦋";                             //Do not change.
+global.author = "Sahan";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/GJSPOP9N0drCErPilB6p7s";
 
 
 //
